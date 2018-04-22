ad9bb974349917617b6cb3074bd50ca0c7503599
SonLaptop\erago <eragonwien@gmail.com>
2018-04-22 12:50:02 +0200

    add empty models

--- api/objects/hero.php
+++ api/objects/hero.php
@@ -12,146 +12,6 @@ class Hero {
     public function __construct($connection) {
         $this->connection = $connection;
     }
-    
-    public function get(){
-        $result = null;
-
-        try {
-            $query = "SELECT * FROM heroes.heroes_full";
-            $statement = $this->connection->prepare($query);
-            $success = $statement->execute();
-
-            // catch execution error here
-            if (!$success) {
-                $errorInfo = $statement->errorInfo();
-                $result['message'] = (isset($errorInfo[2])) ? $errorInfo[2] : null;
-            } else {
-                $result = $statement;
-            }
-        }
-        catch (PDOException $e) {
-            $result['message'] = $e->getMessage();            
-        }
-        finally {
-            return $result;
-        }
-    }
-
-    public function getById(int $id){
-        $result = null;
-
-        try {
-            $query = "SELECT * FROM heroes.heroes_full WHERE id = :id";
-            $statement = $this->connection->prepare($query);
-            $statement->bindParam(':id', $id);
-            $success = $statement->execute();
-
-            // catch execution error here
-            if (!$success) {
-                $errorInfo = $statement->errorInfo();
-                $result['message'] = (isset($errorInfo[2])) ? $errorInfo[2] : null;
-            } else {
-                $result = $statement;
-            }
-        }
-        catch (PDOException $e) {
-            $result['message'] = $e->getMessage();            
-        }
-        finally {
-            return $result;
-        }
-    }
-
-    public function create(){
-        $result = array();
-        try {
-            $query = "INSERT INTO heroes.hero (username, firstname, lastname, gender, image) VALUES (:username, :firstname, :lastname, :gender, :image)";
-
-            $genderInt = ($this->gender) ? 1 : 0;
-
-            $statement = $this->connection->prepare($query);
-            $statement->bindParam(':username', $this->username);
-            $statement->bindParam(':firstname', $this->firstname);
-            $statement->bindParam(':lastname', $this->lastname);
-            $statement->bindParam(':gender', $genderInt);
-            $statement->bindParam(':image', $this->image);
-
-            $success = $statement->execute();
-            $result['success'] = ($success) ? true : false;
-            // catch execution error here
-            if (!$success) {
-                $errorInfo = $statement->errorInfo();
-                $result['message'] = (isset($errorInfo[2])) ? $errorInfo[2] : null;
-            } else {
-                $result['id'] = $this->connection->lastInsertId();
-            }
-        }
-        catch (PDOException $e) {
-            $result['message'] = $e->getMessage();
-        } 
-        finally {
-            return $result;
-        }
-    }
-
-    public function update(){
-        $result = array();
-        try {
-            $query = "UPDATE heroes.hero SET username=:username, firstname=:firstname, lastname=:lastname, gender=:gender, image=:image WHERE id=:id";
-
-            $genderInt = ($this->gender) ? 1 : 0;
-
-            $statement = $this->connection->prepare($query);
-            $statement->bindParam(':id', $this->id);
-            $statement->bindParam(':username', $this->username);
-            $statement->bindParam(':firstname', $this->firstname);
-            $statement->bindParam(':lastname', $this->lastname);
-            $statement->bindParam(':gender', $genderInt);
-            $statement->bindParam(':image', $this->image);
-
-            $success = $statement->execute();
-            $result['success'] = ($success) ? true : false;
-            // catch execution error here
-            if (!$success) {
-                $errorInfo = $statement->errorInfo();
-                $result['message'] = (isset($errorInfo[2])) ? $errorInfo[2] : null;
-            } else {
-                $result['id'] = $this->connection->affectedRows();
-            }
-        }
-        catch (PDOException $e) {
-            $result['message'] = $e->getMessage();
-        } 
-        finally {
-            return $result;
-        }
-    }
-
-    public function deleteById(int $id)
-    {
-        $result = null;
-
-        try {
-            $query = "SELECT * FROM heroes.hero WHERE id = :id";
-            $statement = $this->connection->prepare($query);
-            $statement->bindParam(':id', $id);
-            $success = $statement->execute();
-
-            // catch execution error here
-            if (!$success) {
-                $errorInfo = $statement->errorInfo();
-                $result['message'] = (isset($errorInfo[2])) ? $errorInfo[2] : null;
-            } else {
-                $result['id'] = $this->connection->lastInsertId();
-            }
-        }
-        catch (PDOException $e) {
-            $result['message'] = $e->getMessage();            
-        }
-        finally {
-            return $result;
-        }
-    }
 
     /**
      * Get the value of id
@@ -259,5 +119,172 @@ class Hero {
         $this->image = $image;
 
         return $this;
+    }
+
+    /**
+     * get all heroes
+     * @return array all heroes
+     */
+    public function get(){
+        $result = null;
+
+        try {
+            $query = "SELECT * FROM heroes.heroes_full";
+            $statement = $this->connection->prepare($query);
+            $success = $statement->execute();
+
+            // catch execution error here
+            if (!$success) {
+                $errorInfo = $statement->errorInfo();
+                $result['message'] = (isset($errorInfo[2])) ? $errorInfo[2] : null;
+            } else {
+                $result = $statement;
+            }
+        }
+        catch (PDOException $e) {
+            $result['message'] = $e->getMessage();            
+        }
+        finally {
+            return $result;
+        }
+    }
+
+    /**
+     * get hero by id
+     * @param int id of hero
+     * @return Hero one hero returned
+     */
+    public function getById(int $id){
+        $result = null;
+
+        try {
+            $query = "SELECT * FROM heroes.heroes_full WHERE id = :id";
+            $statement = $this->connection->prepare($query);
+            $statement->bindParam(':id', $id);
+            $success = $statement->execute();
+
+            // catch execution error here
+            if (!$success) {
+                $errorInfo = $statement->errorInfo();
+                $result['message'] = (isset($errorInfo[2])) ? $errorInfo[2] : null;
+            } else {
+                $result = $statement;
+            }
+        }
+        catch (PDOException $e) {
+            $result['message'] = $e->getMessage();            
+        }
+        finally {
+            return $result;
+        }
+    }
+
+    /**
+     * create a new hero
+     * on success, returns last insert id
+     * on failure, returns error message
+     * @return array result of the operation
+     */
+    public function create(){
+        $result = array();
+        try {
+            $query = "INSERT INTO heroes.hero (username, firstname, lastname, gender, image) VALUES (:username, :firstname, :lastname, :gender, :image)";
+
+            $genderInt = ($this->gender) ? 1 : 0;
+
+            $statement = $this->connection->prepare($query);
+            $statement->bindParam(':username', $this->username);
+            $statement->bindParam(':firstname', $this->firstname);
+            $statement->bindParam(':lastname', $this->lastname);
+            $statement->bindParam(':gender', $genderInt);
+            $statement->bindParam(':image', $this->image);
+
+            $success = $statement->execute();
+            $result['success'] = ($success) ? true : false;
+            // catch execution error here
+            if (!$success) {
+                $errorInfo = $statement->errorInfo();
+                $result['message'] = (isset($errorInfo[2])) ? $errorInfo[2] : null;
+            } else {
+                $result['id'] = $this->connection->lastInsertId();
+            }
+        }
+        catch (PDOException $e) {
+            $result['message'] = $e->getMessage();
+        } 
+        finally {
+            return $result;
+        }
+    }
+
+    /**
+     * update a hero
+     * on success, returns affected rows
+     * on failure, returns error message
+     * @return array result of the operation
+     */
+    public function update(){
+        $result = array();
+        try {
+            $query = "UPDATE heroes.hero SET username=:username, firstname=:firstname, lastname=:lastname, gender=:gender, image=:image WHERE id=:id";
+
+            $genderInt = ($this->gender) ? 1 : 0;
+
+            $statement = $this->connection->prepare($query);
+            $statement->bindParam(':id', $this->id);
+            $statement->bindParam(':username', $this->username);
+            $statement->bindParam(':firstname', $this->firstname);
+            $statement->bindParam(':lastname', $this->lastname);
+            $statement->bindParam(':gender', $genderInt);
+            $statement->bindParam(':image', $this->image);
+
+            $success = $statement->execute();
+            $result['success'] = ($success) ? true : false;
+            // catch execution error here
+            if (!$success) {
+                $errorInfo = $statement->errorInfo();
+                $result['message'] = (isset($errorInfo[2])) ? $errorInfo[2] : null;
+            } else {
+                $result['id'] = $this->connection->affectedRows();
+            }
+        }
+        catch (PDOException $e) {
+            $result['message'] = $e->getMessage();
+        } 
+        finally {
+            return $result;
+        }
+    }
+
+    /**
+     * delete a hero
+     * on success, returns affected rows
+     * on failure, returns error message
+     * @return array result of the operation
+     */
+    public function deleteById(int $id)
+    {
+        $result = null;
+
+        try {
+            $query = "SELECT * FROM heroes.hero WHERE id = :id";
+            $statement = $this->connection->prepare($query);
+            $statement->bindParam(':id', $id);
+            $success = $statement->execute();
+
+            // catch execution error here
+            if (!$success) {
+                $errorInfo = $statement->errorInfo();
+                $result['message'] = (isset($errorInfo[2])) ? $errorInfo[2] : null;
+            } else {
+                $result['id'] = $this->connection->affectedRows();
+            }
+        }
+        catch (PDOException $e) {
+            $result['message'] = $e->getMessage();            
+        }
+        finally {
+            return $result;
+        }
     }
 }
